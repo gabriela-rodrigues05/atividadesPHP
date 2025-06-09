861f25a2d2a52b1be00670e9a9c1f57455a3536d
gabriela-rodrigues05 <gabriela.rleite2@senacsp.edu.br>
2025-06-09 10:52:39 -0300

    first commit exercicio 1

---
+++ atvum.php
@@ -0,0 +1,18 @@
+<?php
+    // Comentário de linha com barra dupla
+    # Comentário de linha com cerquilha
+
+    /*
+      Comentário de bloco
+      Este tipo de comentário pode várias linhas
+    */
+
+    // Declaração de variável
+    $nome = "gabi";
+
+    // Concatenação e saída de dados
+    echo "Olá, " . $nome . "!<br>";
+
+    echo "Olá Mundo!<br>"; //Saída com echo
+    print ("Olá Mundo!"); //Saída com print
+?>
